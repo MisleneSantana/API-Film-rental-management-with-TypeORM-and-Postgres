6771105314d313af3ef25cc6e60b5aceea9bab7d
MisleneSantana <contact.mislene@gmail.com>
2023-06-22 13:33:10 +0300

    final commit

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import express, { Application } from 'express';
 import { movieRouter } from './routers/movie.router';
 import { handleError } from './middlewares/handleError.middleware';
 
-export const app: Application = express();
+const app: Application = express();
 app.use(express.json());
 
 app.use('/movies', movieRouter);
 
 app.use(handleError);
+
+export default app;
